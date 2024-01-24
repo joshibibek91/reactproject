5172fb6d6f2f8e2897e95f3467f6aa3ba7c9088d
Bibek <bibek.joshi91@gmail.com>
2024-01-24 09:35:24 +0545

    newchange

--- main.jsx
+++ main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import  ReactDOM from "react-dom/client";
 import App from "./src/App"
 import Header from "./src/pages/Header/Header";
-import "../Project_eCommerce/src/assets/style.scss"
+
 import { BrowserRouter } from "react-router-dom";
 
 
